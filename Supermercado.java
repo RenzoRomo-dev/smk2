6059fcec1769ca057f63dc965b6a309a8e335376
willydevelop <willydev365@gmail.com>
2026-09-25 17:57:55 -0300

    estructura universal del proyecto

--- Supermercado.java
+++ Supermercado.java
@@ -4,28 +4,5 @@ package com.example;
 import java.util.ArrayList;
 
 public class Supermercado {
-    private String productosDisponible;
-    private List<Clientes> listaClientes = new ArrayList<>();
-    private String empleados;
 
-    public boolean agregarProducto(Producto producto){
-
-    };
-
-    public void agregarCliente(Cliente cliente){
-
-
-    };
-
-    public void registarCompra(Cliente cliente, Producto producto){
-
-    };
-
-    public void mostarInventario(){
-
-    }
-
-    public void mostrarCliente(){
-
-    };
 }
