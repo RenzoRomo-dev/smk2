166600a9c1d2ae974ec67f1c8287654ffd48d983
willydevelop <willydev365@gmail.com>
2026-09-25 18:44:26 -0300

    Renzo deja de mandarte cagadas, dicho esto; Avnace en la clase supermercado

--- Supermercado.java
+++ Supermercado.java
@@ -1,8 +1,22 @@
 package com.example;
-
-
 import java.util.ArrayList;
+import java.util.List;
 
 public class Supermercado {
+    private String productosDisponible;
+    private List<Cliente> listaClientes = new ArrayList<>();
+    private List<Empleado> listaEmpleados = new ArrayList<>();
+    private List<Producto> listaPoductos = new ArrayList<>();
+
+    public void agregarProducto(Producto producto) {
+        if (producto != null){
+            listaPoductos.add(producto);
+            System.out.println("producto agregado con exito");
+        } else {
+            System.out.println("el producto no es valido");
+        }
+    };
+
+
 
 }
