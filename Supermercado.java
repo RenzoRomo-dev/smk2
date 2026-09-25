92df06ced570411aae43998d9108c089ec128a25
willydevelop <willydev365@gmail.com>
2026-09-25 17:23:59 -0300

    avance en mi clase

--- Supermercado.java
+++ Supermercado.java
@@ -1,2 +1,31 @@
+package com.example;
+
+
+import java.util.ArrayList;
+
 public class Supermercado {
+    private String productosDisponible;
+    private List<Clientes> listaClientes = new ArrayList<>();
+    private String empleados;
+
+    public boolean agregarProducto(Producto producto){
+
+    };
+
+    public void agregarCliente(Cliente cliente){
+
+
+    };
+
+    public void registarCompra(Cliente cliente, Producto producto){
+
+    };
+
+    public void mostarInventario(){
+
+    }
+
+    public void mostrarCliente(){
+
+    };
 }
